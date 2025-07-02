9cc9237e5b2d930f77c82326cab4e4b40ef7c505
Marcio <marcio9769@gmail.com>
2025-07-02 17:32:21 -0300

    fix: some fixes to change

--- netshoes-frontend/components/dropdown-menu/dropdownmenu.tsx
+++ netshoes-frontend/components/dropdown-menu/dropdownmenu.tsx
@@ -1,4 +1,40 @@
 'use client';
-export default function DropdownMenu() {
-  return <div></div>;
+import React, { useState, useRef, useEffect } from 'react';
+import './style.scss';
+import { CircleUserRound } from 'lucide-react';
+
+export default function UserDropdown() {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => setOpen((prev) => !prev);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  return (
+    <div className="user-dropdown" ref={dropdownRef}>
+      <button className="icon-button" onClick={toggleDropdown}>
+        <CircleUserRound size={24} color="#FFF" />
+      </button>
+      {open && (
+        <div className="dropdown-menu">
+          <div className="arrow" />
+          <ul>
+            <li>Entrar</li>
+            <li>Minha Conta</li>
+            <li>Endereços</li>
+            <li>Minha Netshoes</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
--- netshoes-frontend/components/nav-bar/nav-bar.tsx
+++ netshoes-frontend/components/nav-bar/nav-bar.tsx
@@ -1,19 +1,22 @@
 'use client';
-import { Heart, CircleUserRound } from 'lucide-react';
+import { Heart } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import './style.scss';
+import UserDropdown from '../dropdown-menu/dropdownmenu';
 
 export default function NavBar() {
   return (
     <nav className="nav-container">
-      <Image src="/logo.svg" alt="Netshoes Logo" className="nav-logo" width={160} height={25} />
+      <Link href="/" className="nav-logo-container">
+        <Image src="/logo.svg" alt="Netshoes Logo" className="nav-logo" width={160} height={25} />
+      </Link>
       <div className="nav-end-container">
         <Link href="/wishlist" className="nav-end-container-whishlist">
           <Heart size={24} color="#FFF" />
           Wishlist
         </Link>
-        <CircleUserRound size={24} color="#FFF" />
+        <UserDropdown />
       </div>
     </nav>
   );
